6a98b4996f371d5e50127deb873d7df6dd852fac
Yerridev <cramirezabondan@uss.edu.pe>
2025-04-16 17:53:31 -0500

    frist-borrador

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Archivo', 'sans-serif'], // Agrega la fuente 'Inter' como ejemplo
+            },
+        },
+    },
+    plugins: [],
+};
